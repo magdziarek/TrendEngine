dac0cd33f699e8accdb274914fb8777a09fa02e8
Aleksandra Mag <ale.magdziarek@gmail.com>
2019-10-09 10:40:44 +0200

    "form request shortened"

--- TrendEngine/calculations/routes.py
+++ TrendEngine/calculations/routes.py
@@ -24,30 +24,16 @@ def run_DBEST():
         #parameters for datasets
         parameters = request.form
 
-    print('params', parameters, type(parameters))
     result = dbest_func(parameters)
     return result
 
 @calculations.route('/run_polytrend', methods=['GET', 'POST'])
 def run_polytrend():
     """ Get user's input and send to polytrend_func in polytrend.py"""
-    form = PolyTrendParametersForm()
-    if form.validate_on_submit():
-        flash('Please wait for results. It might take a few minutes...', 'success')
-    else:
-        flash('Please check your input is in the correct form suggested by the placeholders', 'danger')
-
+  
     if request.method == 'POST':
         #parameters for datasets
-        parameters = {}
-        parameters["dataset_name"] = request.form['dataset_name']
-        parameters["date_from"]= request.form['date_from']
-        parameters["date_to"] = request.form['date_to']
-        parameters["coordinates"] = request.form['coordinates']
-        #parameters for PolyTrend
-        parameters["alpha"] = request.form['alpha']
-        parameters["save_result_to_csv"] = request.form['save_result_to_csv']
-        parameters["save_ts_to_csv"] = request.form['save_ts_to_csv']
+        parameters = request.form
 
     result = polytrend_func(parameters)
     return result
